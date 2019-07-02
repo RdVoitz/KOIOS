4f8d0cae48f4824f0f01c795aa5f6133d4513e75
RdVoitz <rdv1u16@soton.ac.uk>
2019-07-02 12:55:49 +0100

    TF-IDF and GENSIM

--- Concepts_Stage1.py
+++ Concepts_Stage1.py
@@ -12,6 +12,9 @@ import requests
 
 
 import pandas as pd
+from scipy import spatial
+from sklearn.feature_extraction.text import TfidfVectorizer
+import gensim
 
 df = pd.read_excel ('koiosDatabase_Concepts_and_Definitions.xlsx') #for an earlier version of Excel, you may need to use the file extension of 'xls'
 #print(df.columns.values)
@@ -40,32 +43,17 @@ usefulAttributes = ['CONCEPT_ID', 'TERM_ID', 'DEFINITION_ID' ,'DEFINITION_CONTEN
 
 for x in df.columns.values:
     if x not in usefulAttributes:
-        df.drop(x, 1, inplace=True)
-        
-print(df.columns.values)
+        df.drop(x, 1, inplace=True)       
+#print(df.columns.values)
 
 
 # In[4]:
 
 
-print(len(df.columns.values))
-
-
-# In[5]:
-
-
-# for index, row in df.iterrows():
-#     term = row['SYNONYM_VALUE']
-#     if term == 'standard form':
-#         print(row['DEFINITION'],'  ', row['CONCEPT_TYPE_ID'], ' ', row['ECCMA_CONCEPT_ID'])
-
-
-# In[6]:
-
+#create a dictionary of terms
 
 keys = set(df['SYNONYM_VALUE'])
 term_to_concepts = dict.fromkeys(keys)
-
 
 for index, row in df.iterrows():
     term = row['SYNONYM_VALUE']
@@ -76,20 +64,15 @@ for index, row in df.iterrows():
     if term_to_concepts[term] == None:
         term_to_concepts[term] = {}
     
+    #use concept id as keys for the dictionary of each term
     term_to_concepts[term][concept_id] = (concept_type, definition)
-
-
-# In[18]:
-
-
-print(len(term_to_concepts['wetting agent']))
-#for x in term_to_concepts['door']:
     
 
 
-# In[7]:
+# In[5]:
 
 
+#delete terms with only one concept
 def deletekeys():
     for x in list(term_to_concepts.keys()):
         if len(term_to_concepts[x]) == 1:
@@ -99,82 +82,40 @@ deletekeys()
 print(len(term_to_concepts))
 
 
-# In[8]:
-
-
-# import nltk
-# nltk.download('stopwords')
-# from nltk.corpus import stopwords
-# from nltk.tokenize import word_tokenize
-
-# stop_words = set(stopwords.words('english'))
-# punctuation = [',','.','!','?',';',':','-']
-
-# def clean(description):
-#     d = description
-#     word_tokens = word_tokenize(d)
-#     fiktered = []
-#     filtered = [w.lower() for w in word_tokens if not w in stop_words and not w in punctuation]
-#     return filtered
-
-
-# In[9]:
-
-
-# example_sent = "this is a Sample sentence, showing off the Stop words filtration"
-# print(clean(example_sent))
-
-
-# In[ ]:
+# In[6]:
 
 
 import spacy
 nlp = spacy.load('en_core_web_lg')
 
 
-# In[67]:
+# In[7]:
 
 
-from scipy import spatial
-from sklearn.feature_extraction.text import TfidfVectorizer
+#Compares two descriptions using the trained tfidf
+#Returns similarity score measured with cosine
 
-def compare(d1,d2):
+def compare(d1,d2, tfidf):
     doc1 = nlp(d1)
     doc2 = nlp(d2)
-    text = [d1,d2]
-#     for t in doc1:
-#         if not t.is_stop | t.is_punct:
-#             print(t.vector)
-#             break
     
-#     doc1 = nlp(' '.join([str(t) for t in doc1 if not t.is_stop | t.is_punct ]))
-#     doc2 = nlp(' '.join([str(t) for t in doc2 if not t.is_stop | t.is_punct ]))
-    
+    #Clean the text
     doc1 = ' '.join([str(t) for t in doc1 if not t.is_stop | t.is_punct ])
     doc2 = ' '.join([str(t) for t in doc2 if not t.is_stop | t.is_punct ])
     
     text = [doc1, doc2]
     
-    
-    tfidf = TfidfVectorizer()
-    tfidf.fit(text)
+    #get the word vector for each description to be compared
     v1 = tfidf.transform([text[0]]).toarray()
     v2 = tfidf.transform([text[1]]).toarray()
     
-   # tfidf.fit()
-#     print(vector.shape)
-#    print(vector.toarray())
-#     print("************************")
-            
-#     v1 = doc1.vector
-#     v2 = doc2.vector
     result = 1 - spatial.distance.cosine(v1,v2)
-    #print(result)
-    #score = doc1.similarity(doc2)  
+
+    #result = doc1.similarity(doc2)  #Using spacy built in similarity
     return result
 
 
-# In[68]:
+# In[8]:
 
 
 # example1 = 'Sachin is a cricket player and a opening batsman'
@@ -190,21 +131,78 @@ def compare(d1,d2):
 # print("E3 & E4 ", compare(example3,example4))
 
 
-# In[ ]:
+# In[9]:
 
 
+# method for training the tf-idf on all of the descriptions
+def trainer():
+    text = []
+    for term in list(term_to_concepts.keys()):
+        for concept in list(term_to_concepts[term]):
+            t, d = term_to_concepts[term][concept]
+            d = ' '.join([str(t) for t in nlp(d) if not t.is_stop | t.is_punct ])
+            text.append(d)
+    tfidf = TfidfVectorizer()
+    tfidf.fit(text)
+    return tfidf
+    
 
 
+# In[12]:
 
-# In[73]:
+
+#GENSIM METHOD
+
+from gensim import corpora
+from gensim.models import TfidfModel
+import numpy as np
+import pprint
+
+def create_dictionary():
+    text = []
+    for term in list(term_to_concepts.keys()):
+            for concept in list(term_to_concepts[term]):
+                t, d = term_to_concepts[term][concept]
+                d = [str(t).lower() for t in nlp(d) if not t.is_stop | t.is_punct ]
+                text.append(d)
+                #d = [d]
+            break
+    dictionary = corpora.Dictionary(text)
+    corpus = [dictionary.doc2bow(doc, allow_update = True) for doc in text]
+
+    tfidf = TfidfModel(corpus, smartirs='ntc')
+    
+    for doc in tfidf[corpus]:
+        print([[dictionary[ids], np.around(freq, decimals=3)] for ids, freq in doc])
+        
+create_dictionary()
 
 
-doublekeys = list(term_to_concepts.keys())
-similar = []
+# In[72]:
+
+
+import datetime
+print(datetime.datetime.now())
+
+#list of terms used as keys
+doublekeys = list(term_to_concepts.keys()) #list of concepts
+
+# list of touples made of similar descriptions
+similar = [] 
+
+#dictionary of
 type_to_similar = dict.fromkeys(set(doublekeys))
 
+#trained tfidf model
+tfidf = trainer()  
+
 for term in doublekeys:
-    smalkeys = list(term_to_concepts[term].keys())
+    smalkeys = list(term_to_concepts[term].keys()) #list of concepts associated with a term
+    
+    #compare pairs of two descriptions by navigating the dictionary
+    #of each term and addint the pairs with a level of similarity above a certain threshold
+    #in a new dictionary associated with the term
+    
     for i in range(0, len(smalkeys)-1):
         conceptid1 = smalkeys[i]
         typeid1, description1 = term_to_concepts[term][conceptid1]
@@ -213,18 +211,19 @@ for term in doublekeys:
             conceptid2 = smalkeys[j]
             typeid2, description2 = term_to_concepts[term][conceptid2]
             
+            #the type of the concepts should be the same for comparison
+            
             if typeid1 == typeid2:
-                if compare(description1,description2) >= 0.90:
-                    similar.append((description1,description2))
+                if compare(description1,description2,tfidf) >= 0.95:
+                    similar.append((description1,description2)) #add pairs to a list just for keeping track 
                     if type_to_similar[term] == None:
                         type_to_similar[term] = []
                     type_to_similar[term] = type_to_similar[term] + [(description1,description2)]
-                    
-                    
-        
+                                       
+print(datetime.datetime.now())        
 
 
-# In[74]:
+# In[73]:
 
 
 print(len(similar))
@@ -234,7 +233,7 @@ print(len(type_to_similar))
 print(len(type_to_similar.keys()))
 
 
-# In[75]:
+# In[76]:
 
 
 def mostSimilar():
@@ -249,12 +248,12 @@ def mostSimilar():
             if len(type_to_similar[term]) > m:
                 memorised = term
                 m = len(type_to_similar[term])
-    print(m)
-    print(memorised)
+    print("Maximum number of similar descriptions for a term  ",m)
+    print("The term for which the max was found  ", memorised)
     print(count)
     print('**************************')
     
-    memorised = "standard form"
+    memorised = "wire"
     
     for touple in type_to_similar[memorised]:
         d1,d2 = touple
