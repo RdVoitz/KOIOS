644951381a4a8e28cf3333108dc59358c808f252
RdVoitz <rdv1u16@soton.ac.uk>
2019-07-15 09:43:56 +0100

    Py version

--- CombinedVersion_TFIDF_TopicModelling.py
+++ CombinedVersion_TFIDF_TopicModelling.py
@@ -51,7 +51,7 @@ def parse_xml(url):
 # final_df = pd.concat(df_list)
 
 
-# In[160]:
+# In[1]:
 
 
 # Import dataset
@@ -80,7 +80,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-# In[161]:
+# In[2]:
 
 
 #data importing
@@ -94,13 +94,13 @@ def read_trim():
     return df
 
 
-# In[170]:
+# In[67]:
 
 
 #tokenizing a description
 #return list of "cleaned" words
 def remove_punct_stop(description):
-    low = [str(t) for t in nlp(description) if not t.is_punct |t.is_stop]
+    low = [str(t) for t in nlp(description) if t.is_alpha and not t.is_stop]
     return low
 
 #create a dictionary of terms
@@ -131,7 +131,7 @@ def term_concepts(df):
     return term_to_concepts, descriptions
 
 
-# In[163]:
+# In[4]:
 
 
 #delete terms with only one concept
@@ -141,7 +141,7 @@ def deletekeys(term_to_concepts):
             del term_to_concepts[x]
 
 
-# In[164]:
+# In[5]:
 
 
 #train and return the bigram generator
@@ -179,7 +179,7 @@ def cleared_text_lists(term_to_concepts, bigram):
     return tfidf_text, topicmodel_text
 
 
-# In[109]:
+# In[34]:
 
 
 #METHODS TESTER
@@ -200,32 +200,32 @@ print(l2)
 # #print(b)
 
 
-# In[165]:
+# In[6]:
 
 
 # method for training the tf-idf on all of the descriptions
-def trainer(text):
-    tfidf = TfidfVectorizer(ngram_range = (1,1), min_df = 10, max_df = 1000 )
-    tfidf.fit(text)
+def trainer(l):
+    tfidf = TfidfVectorizer(ngram_range = (1,1), max_df = 1000 )
+    tfidf.fit(l)
     return tfidf
 
 #train tfidf on descriptions associated with a single term
-def trainer_w_term(term):
-    text = []
-    for concept in list(term_to_concepts[term]):
-            t, d = term_to_concepts[term][concept]
-            d = ' '.join([str(t) for t in nlp(d) if not t.is_stop | t.is_punct ])
-            text.append(d)
-    tfidf = TfidfVectorizer(ngram_range = (1,2))
-    tfidf.fit(text)
-    return tfidf
+# def trainer_w_term(term):
+#     text = []
+#     for concept in list(term_to_concepts[term]):
+#             t, d = term_to_concepts[term][concept]
+#             d = ' '.join([str(t) for t in nlp(d) if not t.is_stop | t.is_punct ])
+#             text.append(d)
+#     tfidf = TfidfVectorizer(ngram_range = (1,2))
+#     tfidf.fit(text)
+#     return tfidf
 
 # Nearest Neighbours training method
-def knn_trainer(tfidf,text):
+def knn_trainer(tfidf,l):
     samples = []
     descriptions = []
     
-    for d in text:
+    for d in l:
         v = tfidf.transform([d]).toarray()
         samples.append(v[0])
         descriptions.append(d)
@@ -239,44 +239,46 @@ def knn_trainer(tfidf,text):
     return nn, descriptions
 
 #return method for KNN
-def return_top_k(k, d,nn, tfidf):
-    d = ' '.join([str(t) for t in nlp(d) if not t.is_stop | t.is_punct ])
-    v = tfidf.transform([d]).toarray()
-    return nn.kneighbors([v[0]])
+def return_top_k(k, d,nn, tfidf, bigram):  
+    sentence =' '.join(bigram[remove_punct_stop(d)])
+    v = tfidf.transform([sentence]).toarray()
+    return nn.kneighbors([v[0]],n_neighbors = k)
      
 
 
-# In[166]:
+# In[7]:
 
 
 #RADU'S TFIDF and KNN Pipeline
-def tfidf_knn():
+def tfidf_knn(l1, bigram, text,k):
     
     #call the tfidf trainer method, return a trained tfidf
     print("STARTED training TF-IDF AT: ",datetime.datetime.now())
     tfidf = trainer(l1)
+    
+    
     print("FINISHED training TF-IDF AT: ",datetime.datetime.now())
     
     #call the knn trainer method, return a trained knn and adjacent list of descriptions
     #in order to visualise the results
     print("STARTED training KNN AT: ", datetime.datetime.now())
-    knn, description = knn_trainer(tfidf,l1)
+    knn, ds = knn_trainer(tfidf,l1)
     print("FINISHED training KNN AT: ",datetime.datetime.now())
 
     #call the method to return the top k -first parameter- most similar
     #descriptions to the one passed as the second parameter
     print("RETURN results for example: ", datetime.datetime.now())
-    top = return_top_k(5,text, knn, tfidf)
+    top = return_top_k(5,text, knn, tfidf, bigram)
     print("Finished at: ", datetime.datetime.now())
 
     #visualise the results
     x,pos = top
     print(top)
     for i in pos[0]:
-        print(description[i])
+        print(ds[i])
 
 
-# In[167]:
+# In[57]:
 
 
 # Step 3: Topic Modelling - LDA Online Training
@@ -295,12 +297,12 @@ def prepare_corpus(preprocessedDescription):
     return dictionary,corpus
 
 # Build the LDA - Online Learning Topic Model
-def create_lda_model(preprocessedDescription):
+def create_lda_model(preprocessedDescription, nr_topics):
     dictionary, corpus = prepare_corpus(preprocessedDescription)
 
     # Online LDA training - processes the whole corpus in one pass, then updates the model, then another pass and so on (Faster Execution)
-    lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=5, random_state=100, update_every=1, chunksize=100, passes=10, alpha='auto', per_word_topics=True)
-    
+    #lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=nr_topics, random_state=100, update_every=1, chunksize=100, passes=10, alpha='auto', per_word_topics=False)
+    lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=nr_topics, random_state=100,passes=10)
     # Different methods of printing key topics and associated key words with scores
     print(lda_model.print_topics())
     #print(lda_model.top_topics(document_term_matrix, dictionary, coherence = 'c_v', topn = words))
@@ -311,7 +313,7 @@ def create_lda_model(preprocessedDescription):
     return lda_model
 
 # Compute perplexity score and coherence score - performance measurement metrics that give an indication of how good the topic model is
-def compute_perplexity_coherence(data_lemmatized):
+def compute_perplexity_coherence(data_lemmatized, lda_model, corpus, dictionary):
     print('\n Perplexity: ', lda_model.log_perplexity(corpus))
 
     coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=dictionary, coherence='c_v')
@@ -353,12 +355,12 @@ def plot_graph(preprocessedDescription, start, limit, step):
         print("Number of Topics =", m, " has Coherence Value of", round(cv, 4))
 
 
-# In[168]:
+# In[19]:
 
 
 #Abhi's Topic Modelling using LDA (Online Training)
 
-def topic_modelling():
+def topic_modelling(l2, nr_topics):
     # Method call - prepare_corpus
     dictionary, corpus = prepare_corpus(l2)
     print(corpus[:1])
@@ -367,13 +369,13 @@ def topic_modelling():
     [[(dictionary[id], freq) for id, freq in corp] for corp in corpus[:1]]
 
     # Method call - lda model creation and training
-    lda_model = create_lda_model(l2)
+    lda_model = create_lda_model(l2, nr_topics)
 
     # Method call - perplexity/coherence score computation
-    compute_perplexity_coherence(l2)
+    compute_perplexity_coherence(l2, lda_model, corpus, dictionary)
 
     # Visualise potential optimum number of topics to determine the diversity of topics and identify closely similar topics, that can be merged into one large topic
-    lda_model = create_lda_model(l2)
+    lda_model = create_lda_model(l2, nr_topics)
     dictionary,corpus = prepare_corpus(l2)
     pyLDAvis.enable_notebook()
     vis = pyLDAvis.gensim.prepare(lda_model, corpus, dictionary)
@@ -384,7 +386,7 @@ def topic_modelling():
     plot_graph(l2, start, limit, step) 
 
 
-# In[171]:
+# In[13]:
 
 
 def main():
@@ -392,26 +394,105 @@ def main():
    #STAGE I - initialise the dataframes, dictionaries, bigrams and list
     print("STAGE I")
     df = read_trim()
+    print("Term-Concept Dictionary is being loaded...")
     term_to_concepts, description = term_concepts(df)
+    print("Bigram model is being trained...")
     bigram = trainBigram(description,1,1)
+    print("Deleting keys")
     deletekeys(term_to_concepts)
+    print("Generate Lists")
     l1, l2 = cleared_text_lists(term_to_concepts, bigram)
     
    #STAGE II - Tfidf
     print("STAGE II")
-    tfidf_knn()
+    k = 5
+    text = "A set of triple-in-line package outline styles with through-hole leads in standard form of which each outline style can be described with the same group of data element types"
+    tfidf_knn(l1,bigram,text,k)
     
    #StageIII - Topic Modelling
     print("STAGE III")
-    topic_modelling()
+    topic_modelling(l2)
 
     
 if __name__ == "__main__":
     main()
 
 
-# In[ ]:
+# In[232]:
 
 
+# return a scipy sparse matrix representing the tfidf weight for each term of each document
+# have to convert into corpus to be loaded for lda model
+inverse =tfidf.fit_transform(l1)
+print(inverse)
 
+
+# In[223]:
+
+
+#KNN trainer for topic modelling
+# numpy array used to store the scores each description has for each topic
+
+import numpy
+def KNN_trainer_topicModelling(lda_model, corpus, no_topics):  
+    no_descriptions = len(corpus)
+    features = numpy.empty(shape=(no_descriptions,no_topics))
+    
+    x = 0
+
+    for sentence in corpus:
+        #print(sentence)
+        numbers = numpy.zeros(no_topics)
+        #print(len(numbers))
+        values = lda_model[sentence]
+        #print(values)
+        for tuples in values:
+            topic, score = tuples
+            numbers[topic] = score
+        features[x] = numbers
+        x = x + 1
+
+    nn_topic = NearestNeighbors(metric = 'l2')#(metric = 'euclidean')
+    nn_topic.fit(features)
+    
+    return nn_topic
+
+
+# In[237]:
+
+
+#running the KNN trainer for topic modelling
+nn = KNN_trainer_topicModelling(lda_model, corpus, 5)
+
+#simulate a new text given as input to the KNN model
+text = "A set of triple-in-line package outline styles with through-hole leads in standard form of which each outline style can be described with the same group of data element types"
+text_vec = dictionary.doc2bow(bigram[remove_punct_stop(text)])
+
+#preprocess the new text
+processed = numpy.zeros(5)
+for tuples in lda_model[text_vec]:
+    topic, score = tuples
+    processed[topic] = score
+    
+#reshape for fitting the kneighbors method for nn
+new = np.reshape(processed, (1,-1))
+top = nn.kneighbors(new, n_neighbors = 5)    
+
+x,pos = top
+print(top)
+for i in pos[0]:
+    print(ds[i])
+    print("************************************************************************")
+
+
+# In[225]:
+
+
+#check the indexes directly from the corpus
+for x in [11445, 12917, 12918, 10982, 11968]:
+    for t in corpus[x]:
+        i, c = t
+        print(dictionary[i], sep=' ', end=' ', flush=True)
+    print()    
+    print("********************")
 
